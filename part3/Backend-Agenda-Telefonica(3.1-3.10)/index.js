8ed98a5108f72c409a34adb9b43938c5a3323445
Tomás <tomasheinzmann09@gmail.com>
2024-09-17 22:25:20 -0300

    Se llego al punto 3.7 del proyecto Agenda telefonica backend

---
+++ part3/Backend-Agenda-Telefonica(3.1-3.10)/index.js
@@ -0,0 +1,105 @@
+const express = require("express")
+const morgan = require("morgan")
+const app = express()
+
+function simpleStringify (object){
+    var simpleObject = {};
+    for (var prop in object ){
+        if (!object.hasOwnProperty(prop)){
+            continue;
+        }
+        if (typeof(object[prop]) == 'object'){
+            continue;
+        }
+        if (typeof(object[prop]) == 'function'){
+            continue;
+        }
+        simpleObject[prop] = object[prop];
+    }
+    return JSON.stringify(simpleObject); 
+};
+
+let people = [
+    { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+    },
+    { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+    },
+    { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+    },
+    { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+    }
+]
+
+app.use(morgan("tiny"))
+
+
+app.get("/api/numbers", (request, response) => {
+    response.json(people)
+})
+
+app.get("/api/numbers/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = people.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get("/api/info", (request, response) => {
+    const nowDate = new Date()
+    response.send(`
+    <div>
+        <p>Phonebook has info for ${people.length}</p>
+        <br/>
+        <p>${nowDate}</p>
+    </div>
+    `)
+})
+
+app.delete("/api/numbers/:id", (request, response) =>{
+    const id = Number(request.params.id)
+    people = people.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+app.use(morgan(":method :url :status :response-time ms"))
+
+app.post("/api/numbers", (request, response) => {
+    const person = JSON.parse(JSON.stringify(request))
+    console.log(person)
+    const name = person.name
+    person.id = Math.floor(Math.random() * 999999)
+    const foundName = people.find((person => person.name === name))
+    if (!person.name || !person.number) {
+        response.status(400).send(
+            {error: "Name or Number not defined"}
+        )
+    } 
+    if (foundName) {
+        response.status(400).send(
+            {error: "Name already in Phonebook"}
+        )
+    } 
+    people = people.concat(person)
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server runing on ${PORT}`)
+})
